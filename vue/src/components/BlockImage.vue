36e0f9212cebeacf0011a705a612a0a65c9d7558
Sit0Ri <yarvik01@yandex.ru>
2022-08-20 00:33:01 +0300

    Добавлен блок фото

---
+++ vue/src/components/BlockImage.vue
@@ -0,0 +1,25 @@
+<template>
+	<BlockMovable>
+        <img :src="require('@/assets/image/billy.jpg')" alt="" draggable="false" >
+    </BlockMovable>
+</template>
+
+<script>
+import BlockMovable from '@/components/MovableBlock.vue'
+
+export default
+{
+    components: { BlockMovable },
+}
+
+</script>
+
+<style scoped>
+img
+{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+}
+</style>
