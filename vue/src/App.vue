849164383d0c3fc8afc9a62f4f7ba188c8af5445
Sergey <grushko_serhii@mail.ru>
2022-08-19 20:28:16 +0300

    Фронтовая часть

---
+++ vue/src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="all"> 
+    <!-- Меню с кнопками -->
+    <WidjetsMenu @addText="addText"></WidjetsMenu>
+    <!-- Общее поле, где можно перемещать элементы -->
+    <div
+      class="container"
+    >
+    <!-- Блок с текстом -->
+    <!-- id, xCoord, yCoord это пропсы из TextBlock.vue -->
+      <div v-if="items.length > 0">
+        <MovableBlock
+          v-for="item in items"
+          :key="item.id"
+          :id="item.id"
+          :selected-object-id="selectedObjectId"
+          @object-selection="objectSelection"
+        ></MovableBlock>
+      </div>
+    </div>
+    <div class="objectMenu" id="optionsMenu">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+import MovableBlock from '@/components/MovableBlock.vue'
+import WidjetsMenu from '@/components/WidjetsMenu.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    MovableBlock,
+    WidjetsMenu,
+  },
+  data (){
+    return{
+      // Массив объектов, где хранятся данные об элементе
+      items: [],
+      selectedObjectId : 0,
+    }
+  },
+  methods: {
+    addText () { // Добавление текстового блока
+      this.items.push({ // Запись нового элемента в массив объектов
+        id: this.items.length, 
+      })
+    },
+    objectSelection (id) {
+      this.selectedObjectId = id
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin: 0;
+}
+
+.container{
+  background-color: #FFFFFF;
+  height:800px;
+  width: 72%;
+}
+.all{
+  display: flex;
+  justify-content: space-between;
+}
+
+body{
+  margin: 0;
+  padding: 0;
+}
+
+.objectMenu{
+  background-color: blueviolet;
+  width: 20%;
+  border: 2px solid black;
+}
+</style>
---
+++ vue/src/components/WidjetsMenu.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="menu">
+        <div class="buttonContainer" @click="addText">
+            <img src="@/assets/image/iconBackgroud.png">
+            <p>Задний фон</p>
+        </div>
+        <div class="buttonContainer" @click="addText">
+            <img src="@/assets/image/iconText.png">
+            <p>Текст</p>
+        </div>
+        <div class="buttonContainer">
+            <img src="@/assets/image/iconImage.png">
+            <p>Картинка</p>
+        </div>
+        <div class="buttonContainer">
+            <img src="@/assets/image/iconFigure.png">
+            <p>Фигура</p>
+        </div>
+        <div class="buttonContainer">
+            <img src="@/assets/image/iconSlider.png">
+            <p>Слайдер</p>
+        </div>
+        <div class="buttonContainer">
+            <img src="@/assets/image/iconForm.png">
+            <p>Форма</p>
+        </div>
+        <div class="buttonContainer">
+            <img src="@/assets/image/iconButton.png">
+            <p>Кнопка</p>
+        </div>
+        <div class="buttonContainer">
+            <img src="@/assets/image/iconInput.png">
+            <p>Поле ввода</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'WidjetsMenu',
+       
+        methods: {
+           addText(){
+            this.$emit('addText')
+           }
+        }
+    }
+</script>
+
+<style scoped>
+    .menu{
+        /* padding: 15px; */
+        background-color: #1C2A33;
+        width: 10%;
+    }
+
+    .buttonContainer{
+        margin: 7% 20% 0% 20%; /* top | right | bottom | left */
+        width: 60%;
+        background-color: #263845;
+        text-align: center;
+        padding-bottom: 5%;
+    }
+
+    .buttonContainer:hover{
+        background-color: #49677C;
+    } 
+
+    p{
+        color: #D4E6ED;
+        font-family: Arial, Helvetica, sans-serif;
+        src: local('Extra light italic'),
+		url(@/assets/Exo_2_Extra_Light.ttf); 
+        font-size: 0.8em;
+        margin-top: 10%;
+        margin-bottom: 0; 
+    }
+
+    img{
+        margin-top: 10%;
+    }
+
+    button{
+        height: 50px;
+        margin-right: 10px;
+    }
+</style>
