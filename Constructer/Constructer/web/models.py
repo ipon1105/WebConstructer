dd136137050445ac20e6d948d4c0ef1991f4dbd8
ipon1105 <71529066+ipon1105@users.noreply.github.com>
2022-08-20 18:47:11 +0300

    Add new matrix

--- Constructer/Constructer/web/models.py
+++ Constructer/Constructer/web/models.py
@@ -24,3 +24,30 @@ class Project(models.Model):
         pass
 
     pass
+
+
+class Widget(models.Model):
+    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    single = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+        pass
+    pass
+
+
+class WidgetProperty(models.Model):
+    parrentId = models.BigIntegerField()
+    title = models.CharField(max_length=50)
+    field = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    measure = models.CharField(max_length=10)
+    defaultValue = models.CharField(max_length=25)
+    values = CustomJsonField(default=["none", ""])
+    additionalData = CustomJsonField(default=['#ffffff', '#000000', 'rgb(255, 120, 0)'])
+
+    def __str__(self):
+        return self.title
+        pass
+    pass
