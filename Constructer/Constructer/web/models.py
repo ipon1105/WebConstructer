3c086631e3540aa5e842c72dee3539047848cb3a
ipon1105 <71529066+ipon1105@users.noreply.github.com>
2022-08-20 15:06:01 +0300

    edit

--- Constructer/Constructer/web/models.py
+++ Constructer/Constructer/web/models.py
@@ -1,17 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import JSONField
 from django.db import models
-import uuid
 
 
 class User(AbstractUser):
     pass
 
 
-class Project(models.Model):
-    user_id = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
+class Project2(models.Model):
+    user_id = models.BigIntegerField(primary_key=False)
     title = models.CharField(max_length=25)
     data = JSONField()
+
 
     def __str__(self):
         return self.user_id
