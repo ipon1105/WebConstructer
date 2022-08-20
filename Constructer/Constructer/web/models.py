6f1459fba30680e4889ecd3335484f3f2c3034cf
ipon1105 <71529066+ipon1105@users.noreply.github.com>
2022-08-20 13:53:58 +0300

    Add new table to BD

--- Constructer/Constructer/web/models.py
+++ Constructer/Constructer/web/models.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.db.models import JSONField
 from django.db import models
+import uuid
 
-# Create your models here.
+
 class User(AbstractUser):
+    pass
+
+
+class Project(models.Model):
+    user_id = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=25)
+    data = JSONField()
+
+    def __str__(self):
+        return self.user_id
+        pass
 
     pass
